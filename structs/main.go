498618244b4f28bcf994c98b30f85b938c11ebd8
quochieng <aiyanarich64@gmail.com>
2024-11-19 05:03:08 +0300

    fead: structs declaration and methods

--- structs/main.go
+++ structs/main.go
@@ -7,12 +7,20 @@ type Person struct {
 	Age  int
 }
 
+func (n *Person) UpdateName(name string) {
+	n.Name = name
+}
+
 func main() {
 
-	man := Person{Name: "queen", Age: 12}
-	// output will be {queen 12}
-	fmt.Printf("%v\n", man)
-	//output will be {Name:queen Age:12}
-	fmt.Printf("%+v\n", man)
+	name := Person{
+		Name: "Quinter",
+		Age:  12,
+	}
+	//fmt.Printf("%v") : output of struct will be {"Quinter 12"}
+	//using %+v gives  this output includes fields of structs and assigned values fmt.printf("%+v") {Name:Quinter Age:12}
+	fmt.Printf("first instance of struct %+v\n", name)
+	name.UpdateName("Apondi")
+	fmt.Printf("second intance of struct after struct %+v\n", name)
 
 }
